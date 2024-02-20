b407fe61f93f98da70ca0046fee347bc5db4cada
Dron Kishor <dronkishor2002@gmail.com>
2024-02-21 01:05:00 +0530

    Readme

--- email_frontend/src/App.jsx
+++ email_frontend/src/App.jsx
@@ -7,7 +7,9 @@ function App() {
 
   return (
     <>
-
+    <h1>Welcome to Email Validator</h1>
+  <h2>This Web-App provides validation for your emails. <br /> It checks whether the email you entered exists on the internet or not.</h2>
+  <h3>Just type your email in the input field and press Enter to validate.</h3>
     <form action="">
         <input type="email" placeholder='Email' name='email' value={state.email} onChange={(e)=>handleData(e)} style={{width:200,height:50,margin:20,padding:'0px 10px'}}/>
         <br />
